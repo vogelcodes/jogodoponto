e90bfe82f0a6710c312a8c12c281045d898e907e
Daniel <danielcvt@gmail.com>
2019-10-21 16:31:41 -0300

    socket.io chat crewrpg

--- public/chat.js
+++ public/chat.js
@@ -1,5 +1,5 @@
 // Make connection
-var socket = io.connect('http://localhost:3000');
+var socket = io.connect('http://crewrpg-com.umbler.net:3000');
 
 //Query DOM
  var message = document.getElementById('message'),
