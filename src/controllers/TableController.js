1e5903375c11851d28bdd4ab730a3f56ee7d48db
Daniel <danielcvt@gmail.com>
2019-10-30 02:36:53 -0300

    jogoponto

--- src/controllers/TableController.js
+++ src/controllers/TableController.js
@@ -4,15 +4,21 @@ module.exports = {
   async store(req, res) {
     const { tableName, size } = req.body;
     const { user_id } = req.headers;
-    let table = await Table.create({
-      tableName: tableName,
-      size: size,
-      gameState: {
-        board: "initial"
-      },
-      user: user_id
-    });
 
+    let table = await Table.findOne({
+      user: user_id,
+      tableName: tableName
+    });
+    if (!table) {
+      table = await Table.create({
+        tableName: tableName,
+        size: size,
+        gameState: {
+          board: "initial"
+        },
+        user: user_id
+      });
+    }
     return res.json(table);
   }
 };
