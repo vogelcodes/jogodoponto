b1011c83baeb4105a704881e991bc96f958671ea
Daniel <danielcvt@gmail.com>
2019-10-30 00:27:29 -0300

    jogodoponto

--- src/server.js
+++ src/server.js
@@ -1,8 +1,9 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const routes = require("./routes");
 
 const app = express();
 
-app.get("/", (req, res) => {
-  return res.send("Jogo do ponto");
-});
+app.use(express.json());
+app.use(routes);
 app.listen(3000);
