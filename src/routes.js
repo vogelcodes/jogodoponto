4d6c9c56703ea43c05aff86c9ebaf7b3f3000e12
Daniel <danielcvt@gmail.com>
2019-10-30 00:56:47 -0300

    jogodoponto

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,11 @@
 const express = require("express");
+const SessionController = require("./controllers/SessionController");
+
 const routes = express.Router();
 
 routes.get("/", (req, res) => {
   return res.json({ texto: "Jogo do Ponto" });
 });
+routes.post("/sessions", SessionController.store);
 
 module.exports = routes;
--- src/server.js
+++ src/server.js
@@ -3,6 +3,13 @@ const mongoose = require("mongoose");
 const routes = require("./routes");
 
 const app = express();
+mongoose.connect(
+  "mongodb+srv://vogelcodes:vogelcodes18@vogelcodes-kpzli.mongodb.net/jogodoponto?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }
+);
 
 app.use(express.json());
 app.use(routes);
